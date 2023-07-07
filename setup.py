0f5ad50fd6a21680320fe96c57456947720884f7
jpark <jpark@keti.re.kr>
2023-07-07 18:09:24 +0900

    update

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup
 
+setup(
+    name='evc',
+    version='0.0.1',
+    description='todo',
+    url='todo.ac.kr',
+    author='evc',
+    author_email='todo',
+    licence='MIT',
+    packages=['lib'],
+    zip_safe=False,
+    install_requires=[
+        'numpy'
+    ]
+)
